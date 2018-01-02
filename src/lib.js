474dfd681a2b3dc92b72d9ffaa023c7e7fb09ecb
ldiego <lfdiego7@gmail.com>
2018-01-02 01:01:11 -0500

    Se agregan archivos

---
+++ src/lib.js
@@ -0,0 +1,7 @@
+exports.print = function () {
+  console.log("This is a message from the library package");
+}
+
+exports.print2 = function () {
+  console.log("This is a message 2 from the library package");
+}
